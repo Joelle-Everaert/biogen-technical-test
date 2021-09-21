d84ac39514b9ef46f3730a91a7ee108d5fb5adc8
Joëlle <72984240+Joelle-Everaert@users.noreply.github.com>
2021-09-21 10:40:35 +0200

    add breakpoints for responsive

---
+++ src/utils/responsive/breakpoints.js
@@ -0,0 +1,11 @@
+const size = {
+    mobile: '375px',
+    tablet: '768px',
+    laptop: '992px',
+  }
+  
+  export const device = {
+    mobile: `(min-width: ${size.mobileM})`,
+    tablet: `(min-width: ${size.tablet})`,
+    laptop: `(min-width: ${size.laptop})`,
+  };
