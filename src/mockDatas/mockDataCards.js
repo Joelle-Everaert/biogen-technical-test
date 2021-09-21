acd4d0afd155a288b2daf689cb8f16dc6edf801f
Joëlle <72984240+Joelle-Everaert@users.noreply.github.com>
2021-09-21 10:22:32 +0200

    add pictures and mockdatas

---
+++ src/mockDatas/mockDataCards.js
@@ -0,0 +1,38 @@
+import card1  from '../images/card01.png'
+import card2  from '../images/card02.png'
+import card3  from '../images/card03.png'
+import card4  from '../images/card04.png'
+
+
+const mockedCards = [
+    {
+      title: 'Neurogenium Aktuell',
+      score: 4.5,
+      tag: 'webinar',
+      follow: 7,
+      image: card1,
+    },
+    {
+      title: 'Experten-Interviews zur SMA',
+      score: 4.7,
+      tag: 'interviews',
+      follow: 5,
+      image: card2,
+    },
+    {
+      title: 'Nusinersen – Wirksamkeit bei 5q-SMA ist keine Frage des Alters',
+      score: 4.1,
+      tag: 'symposium',
+      follow: 9,
+      image: card3,
+    },
+    {
+      title: 'Lesen und Lesen lassen',
+      score: 4.0,
+      tag: 'explainer',
+      follow: 3,
+      image: card4,
+    },
+  ];
+
+  export default mockedCards
---
+++ src/mockDatas/mockDataVideos.js
@@ -0,0 +1,50 @@
+import episode1  from '../images/video01.jpg'
+import episode2  from '../images/video02.jpg'
+import episode3  from '../images/video03.jpg'
+import episode4  from '../images/video04.jpg'
+import episode5  from '../images/video05.jpg'
+import episode6  from '../images/video06.jpg'
+
+
+
+const mockedVideoPreviews = [
+    {
+      title: 'MS und Familienplanung Immuntherapien bei MS in ...',
+      completedTime: '2:42',
+      tag: 'episode 4',
+      image: episode4,
+    },
+    {
+      title: 'MS und Familienplanung MS-Aktivität in der Schwangerschaft und danach',
+      completedTime: '1:30',
+      tag: 'episode 3',
+      image: episode3,
+    },
+    {
+      title: 'Unter der Lupe – Können wir für Alzheimer von der MS lernen?',
+      completedTime: '0:46',
+      tag: 'episode 2',
+      image: episode2,
+    },
+    {
+      title: 'Wechselwirkungen in der MS Therapie - Klinik und Pharmakologie',
+      completedTime: '3:23',
+      tag: 'episode 1',
+      image: episode1
+    },
+    {
+      title: 'Impfen bei MS zu Coronazeiten',
+      completedTime: '1:14',
+      tag: 'episode 5',
+      image: episode5
+    },
+    {
+      title: 'MS bei Kindern und im Alter',
+      completedTime: '0:42',
+      tag: 'episode 6',
+      image: episode6,
+    },
+    
+  ];
+ 
+  export default mockedVideoPreviews
